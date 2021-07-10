8e10fc06553aee424c0c117ec4ed1eff9b8e8f86
Sakib Sami <s4kibs4mi@gmail.com>
2021-07-10 16:50:04 +0600

    - Added mock test for UpdateUser

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Example project to show, how to implement mock test in Golang.
 
-Full Tutorial: [Visit]()
+Full Tutorial: [Visit](https://sakib.ninja/golang-how-to-mock-test/)
--- services/mocks/mock_user_service_test.go
+++ services/mocks/mock_user_service_test.go
@@ -23,7 +23,27 @@ func Test_UserService_CreateUser(t *testing.T) {
 	u, err := mockUserService.CreateUser(expectedName, expectedEmail)
 	require.NoError(t, err)
 
-	require.Equal(t, expectedUser.ID, "2")
+	require.Equal(t, expectedUser.ID, u.ID)
+	require.Equal(t, expectedUser.Name, u.Name)
+	require.Equal(t, expectedUser.Email, u.Email)
+}
+
+func Test_UserService_UpdateUser(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	expectedID := "1"
+	expectedName := "Sakib"
+	expectedEmail := "sakib@liveklass.io"
+	expectedUser := &services.User{ID: expectedID, Name: expectedName, Email: expectedEmail}
+
+	mockUserService := NewMockUserService(ctrl)
+	mockUserService.EXPECT().UpdateUser(expectedID, expectedName, expectedEmail).Return(expectedUser, nil)
+
+	u, err := mockUserService.UpdateUser(expectedID, expectedName, expectedEmail)
+	require.NoError(t, err)
+
+	require.Equal(t, expectedUser.ID, u.ID)
 	require.Equal(t, expectedUser.Name, u.Name)
 	require.Equal(t, expectedUser.Email, u.Email)
 }
